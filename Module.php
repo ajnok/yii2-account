e08291bea84ff77cface9acc9747cbe8712109c5
Aj.Nok <ajnokit@gmail.com>
2016-05-30 13:55:05 +0700

    Preparing...

---
+++ Module.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alisa
+ * Date: 30/5/2559
+ * Time: 13:34
+ */
+
+namespace ajnok\account;
+
+use dektrium\user\Module as BaseModule;
+
+class Module extends BaseModule
+{
+    public function init()
+    {
+        parent::init();
+    }
+}
